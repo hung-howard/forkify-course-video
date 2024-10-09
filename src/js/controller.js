f163a4aa3f9d7eea1cf6d15fe890e5be5dafcf19
HowardHung <howard1980041@gmail.com>
2024-10-09 16:36:08 +0800

    cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -129,10 +129,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the APP');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -141,6 +137,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
