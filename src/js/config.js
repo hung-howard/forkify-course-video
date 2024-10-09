7c3c65e190904664956bbfd31d056c6a552d7c38
HowardHung <howard1980041@gmail.com>
2024-10-09 15:31:26 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a5798ac8-6cca-48f0-bb99-4be06ab16a8d';
+export const MODAL_CLOSE_SEC = 2.5;
